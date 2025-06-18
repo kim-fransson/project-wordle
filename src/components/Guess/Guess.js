416b3949c4f9f47e31c7e56c819fbdc47e5627ba
Kim Fransson <45737053+kim-fransson@users.noreply.github.com>
2025-06-18 22:40:36 +0200

    exercise 4 done (#4)

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,23 @@
 import { WORD_LENGTH } from "../../constants";
+import { checkGuess } from "../../game-helpers";
 import { range } from "../../utils";
 
-function Guess({ value }) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : "cell";
+  return <span className={className}>{letter}</span>;
+}
+
+function Guess({ value, answer }) {
+  const result = checkGuess(value, answer);
+
   return (
     <p className="guess">
       {range(WORD_LENGTH).map((index) => (
-        <span key={index} className="cell">
-          {value && value[index]}
-        </span>
+        <Cell
+          key={index}
+          letter={result ? result[index].letter : undefined}
+          status={result ? result[index].status : undefined}
+        />
       ))}
     </p>
   );
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -2,11 +2,11 @@ import { range } from "../../utils";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import Guess from "../Guess";
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((index) => (
-        <Guess key={index} value={guesses[index]?.value} />
+        <Guess key={index} value={guesses[index]?.value} answer={answer} />
       ))}
     </div>
   );
