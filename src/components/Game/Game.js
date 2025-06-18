e4256b33a59f68ee529fc78c15eafff576f04322
Kim Fransson <kim.fransson.22@gmail.com>
2025-06-18 20:13:18 +0200

    fixed import in Game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
-import GuessForm from "../GuessForm/GuessForm";
+import GuessForm from "../GuessForm";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
