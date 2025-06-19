73069a1f51fc422fb045a16cbd17f14ca37baa4c
Kim Fransson <kim.fransson.22@gmail.com>
2025-06-19 13:57:56 +0200

    keyboard stretchgoal done

---
+++ src/components/Keyboard/Keyboard.constants.js
@@ -0,0 +1,11 @@
+export const KEYBOARD_KEYS = [
+  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+  ["Z", "X", "C", "V", "B", "N", "M"],
+];
+
+export const STATUS_PRIORITY = Object.freeze({
+  incorrect: 1,
+  misplaced: 2,
+  correct: 3,
+});
