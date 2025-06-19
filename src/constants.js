ddbd8f8cbd9445eeef025b5387bceecb306cc19b
Kim Fransson <45737053+kim-fransson@users.noreply.github.com>
2025-06-19 10:24:07 +0200

    exercise 5 done (#5)

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,8 @@
 export const WORD_LENGTH = 5;
 export const NUM_OF_GUESSES_ALLOWED = 6;
+
+export const GameStatus = Object.freeze({
+  RUNNING: Symbol("running"),
+  WON: Symbol("won"),
+  LOST: Symbol("lost"),
+});
